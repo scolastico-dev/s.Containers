7df5074d7a46f65c469b027f6306c1667a0aed7b
Joschua Becker <jb@scolasti.co>
2022-08-12 09:06:54 +0200

    init

---
+++ cron/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ -n "$CRON_TZ" ]; then
+  echo "Changing timezone to $CRON_TZ"
+  export TZ=$CRON_TZ
+fi
+
+if [ -n "$CRON_STARTUP" ]; then
+  echo "Running CRON_STARTUP: $CRON_STARTUP"
+  $CRON_STARTUP
+fi
+
+echo "Preparing cron..."
+# load every env var beginning with "JOB_" and register it as a cron job
+for env_var in "${!JOB_@}"; do
+  echo "Registering cron job: ${!env_var}"
+  crontab -l | { cat; echo "${!env_var}"; } | crontab -
+done
+
+echo "Starting cron..."
+cron -f
