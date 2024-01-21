fdd2f8c6e3ac046d9317e42cc567cab769534c09
Joschua Becker <jb@scolasti.co>
2024-01-21 11:42:31 +0100

    fix: added type conversion to string

--- src/email-lexoffice-import/index.js
+++ src/email-lexoffice-import/index.js
@@ -96,7 +96,7 @@ const redirectUnparsable = async (mail, id) => {
     attachments: [
       {
         filename: 'mail.eml',
-        content: mail,
+        content: String(mail),
       }
     ]
   })
