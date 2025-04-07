e50ab14f45fabfa8bf28a7e183f9ddd9e87d93f5
Joschua Becker <jb@scolasti.co>
2025-04-07 12:04:35 +0200

    fix: added body parser limit

--- src/deployment-api/src/main.ts
+++ src/deployment-api/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    rawBody: true,
+  });
+  app.useBodyParser('raw', { limits: "500mb" });
   await app.listen(3000);
 }
 bootstrap().then();
