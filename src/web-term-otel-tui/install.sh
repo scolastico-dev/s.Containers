75a3e358e0cccd7a93369f18a8678dcf2c1033c3
Joschua Becker <jb@scolasti.co>
2025-07-16 11:12:14 +0200

    feat: updated web term otel install script

--- src/web-term-otel-tui/install.sh
+++ src/web-term-otel-tui/install.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
 ARCH=$(uname -m)
+VERSION="v0.4.1"
+BASE_URL="https://github.com/ymtdzzz/otel-tui/releases/download"
 if [ "$ARCH" = "x86_64" ]; then
-  curl -L -o otel-tui.tar.gz https://github.com/ymtdzzz/otel-tui/releases/download/v0.4.1/otel-tui_Linux_x86_64.tar.gz
+  curl -L -o otel-tui.tar.gz "$BASE_URL/$VERSION/otel-tui_Linux_x86_64.tar.gz"
 elif [ "$ARCH" = "aarch64" ]; then
-  curl -L -o otel-tui.tar.gz https://github.com/ymtdzzz/otel-tui/releases/download/v0.4.1/otel-tui_Linux_arm64.tar.gz
+  curl -L -o otel-tui.tar.gz "$BASE_URL/$VERSION/otel-tui_Linux_arm64.tar.gz"
 else
   echo "Unsupported architecture: $ARCH"
   exit 1
