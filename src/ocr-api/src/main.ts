ad31b85f9d62f10ab81130ae1f98e8dbbceb7bcf
Joschua Becker <jb@scolasti.co>
2025-12-30 21:49:33 +0100

    feat: added persistAuthorization to swagger

--- src/ocr-api/src/main.ts
+++ src/ocr-api/src/main.ts
@@ -11,7 +11,9 @@ async function bootstrap() {
     .addBasicAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('swagger', app, document);
+  SwaggerModule.setup('swagger', app, document, {
+    swaggerOptions: { persistAuthorization: true },
+  });
 
   await app.listen(process.env.PORT ?? 3000);
 }
